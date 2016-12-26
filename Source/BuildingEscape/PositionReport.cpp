d7feb5d5e4a078d248de4757223f3ed915db5c98
Conal <conalsmith49@gmail.com>
2016-12-26 01:57:14 +0000

    BE59 Get component name and output to log

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -19,8 +19,9 @@ UPositionReport::UPositionReport()
 void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
-
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on Chair!"));
+	
+	FString ObjectName = GetOwner()->GetName();
+	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on %s"), *ObjectName);
 	
 }
 
